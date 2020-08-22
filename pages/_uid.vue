719c08f3885464260633a91cd2496130d477d1dc
Ubuntu <vincent.tsn@gmail.com>
2020-08-22 13:47:13 +0000

    initial commit

---
+++ pages/_uid.vue
@@ -0,0 +1,16 @@
+<template>
+    <slice-zone
+        type="page"
+        :uid="$route.params.uid"
+    />
+</template>
+
+<script>
+import SliceZone from 'vue-slicezone'
+
+export default {
+    components: {
+        SliceZone
+    },
+}
+</script>
